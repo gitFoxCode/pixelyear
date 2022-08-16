6666b2eae27bcd5c4d4d1d05b7e1d2b9e4b4380d
Sebastian <sebeklis132@gmail.com>
2022-08-16 22:42:38 +0200

    +stats page

--- pages/stats/index.vue
+++ pages/stats/index.vue
@@ -1,3 +1,26 @@
 <template>
-    
+    <section class="section">
+        <TopNav />
+        <main>
+            <div class="cards">
+                <div class="card">
+                    <span class="title">2022</span>
+                    <p>Statistics from 2022</p>
+                    
+                    [dashboard]
+                    [rate]
+                    [health]
+                </div>
+            </div>
+        </main>
+    </section>
 </template>
+
+<style lang="scss" scoped>
+main{
+    padding-top: 6rem;
+}
+.cards{
+    display: flex;
+}
+</style>
